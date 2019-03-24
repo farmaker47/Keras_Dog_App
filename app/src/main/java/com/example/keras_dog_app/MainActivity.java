9edc8d50caf9e4505ab46dc49c40764d47fa8ba4
farmaker47 <farmaker47@yahoo.cm>
2019-03-24 18:41:42 +0200

    Compute the first image unsuccesfully with wrong input.

--- app/src/main/java/com/example/keras_dog_app/MainActivity.java
+++ app/src/main/java/com/example/keras_dog_app/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.keras_dog_app;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,8 +11,37 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.keras_dog_app.Utils.ImageUtils;
+
+import org.tensorflow.contrib.android.TensorFlowInferenceInterface;
+
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Load the tensorflow inference library
+    static {
+        System.loadLibrary("tensorflow_inference");
+    }
+
+    //PATH TO OUR MODEL FILE AND NAMES OF THE INPUT AND OUTPUT NODES
+    private String MODEL_PATH = "file:///android_asset/keras_dogs.pb";
+    private String INPUT_NAME = "global_average_pooling2d_1_input";
+    private String OUTPUT_NAME = "output_1";
+    private TensorFlowInferenceInterface tf;
+
+    //ARRAY TO HOLD THE PREDICTIONS AND FLOAT VALUES TO HOLD THE IMAGE DATA
+    float[] PREDICTIONS = new float[1000];
+    private float[] floatValues;
+    /*private int[] INPUT_SIZE = {224,224,3};*/
+    private int[] INPUT_SIZE = {224,224,3};
+
+    private TextView resultView;
+    private ImageView imageView;
+    private Snackbar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +50,41 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        //initialize tensorflow with the AssetManager and the Model
+        tf = new TensorFlowInferenceInterface(getAssets(),MODEL_PATH);
+
+        resultView = findViewById(R.id.textViewResult);
+        imageView = findViewById(R.id.imageViewPhoto);
+
+        progressBar = Snackbar.make(imageView,"PROCESSING IMAGE",Snackbar.LENGTH_INDEFINITE);
+
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                /*Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();*/
+
+                try{
+
+                    //READ THE IMAGE FROM ASSETS FOLDER
+                    InputStream imageStream = getAssets().open("labrador.jpg");
+
+                    Bitmap bitmap = BitmapFactory.decodeStream(imageStream);
+
+                    imageView.setImageBitmap(bitmap);
+
+                    progressBar.show();
+
+                    predict(bitmap);
+                }
+                catch (Exception e){
+
+                }
             }
         });
+
+
     }
 
     @Override
@@ -48,5 +107,99 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    //FUNCTION TO COMPUTE THE MAXIMUM PREDICTION AND ITS CONFIDENCE
+    public Object[] argmax(float[] array){
+
+
+        int best = -1;
+        float best_confidence = 0.0f;
+
+        for(int i = 0;i < array.length;i++){
+
+            float value = array[i];
+
+            if (value > best_confidence){
+
+                best_confidence = value;
+                best = i;
+            }
+        }
+
+        return new Object[]{best,best_confidence};
+
+
+    }
+
+    //Class that takes in a bitmap and performs inference
+    public void predict(final Bitmap bitmap){
+
+
+        //Runs inference in background thread
+        new AsyncTask<Integer,Integer,Integer>(){
+
+            @Override
+
+            protected Integer doInBackground(Integer ...params){
+
+                //Resize the image into 224 x 224
+                Bitmap resized_image = ImageUtils.processBitmap(bitmap,224);
+
+                //Normalize the pixels
+                floatValues = ImageUtils.normalizeBitmap(resized_image,224,127.5f,1.0f);
+
+                //Pass input into the tensorflow
+                tf.feed(INPUT_NAME,floatValues,1,224,224,3);
+
+                //compute predictions
+                tf.run(new String[]{OUTPUT_NAME});
+
+                //copy the output into the PREDICTIONS array
+                tf.fetch(OUTPUT_NAME,PREDICTIONS);
+
+                //Obtained highest prediction
+                Object[] results = argmax(PREDICTIONS);
+
+
+                int class_index = (Integer) results[0];
+                float confidence = (Float) results[1];
+
+
+                try{
+
+                    final String conf = String.valueOf(confidence * 100).substring(0,5);
+
+                    //Convert predicted class index into actual label name
+                    final String label = ImageUtils.getLabel(getAssets().open("labels.json"),class_index);
+
+
+
+                    //Display result on UI
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+
+                            progressBar.dismiss();
+                            resultView.setText(label + " : " + conf + "%");
+
+                        }
+                    });
+
+                }
+
+                catch (Exception e){
+
+
+                }
+
+
+                return 0;
+            }
+
+
+
+        }.execute(0);
+
     }
 }
